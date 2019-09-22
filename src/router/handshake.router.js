488bc402a1bf79d77866068108450ffd1393ad45
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-09-22 20:41:46 +0100

    adds handshake response to queuedHandshakes

--- src/router/handshake.router.js
+++ src/router/handshake.router.js
@@ -1,19 +1,44 @@
 const express = require('express')
 const router = express.Router()
 const {
-  remove
+  remove,
+  findIndex
 } = require('lodash')
 
 const queuedHandshakes = []
 
 router.post('/', (req, res) => {
-  const handshake = req.body.handshake
+  const {
+    handshake
+  } = req.body
   const alreadyQueued = queuedHandshakes.includes(handshake)
   if (!alreadyQueued) {
-    queuedHandshakes.push(req.body.handshake)
-    res.send(200, 'Handshake queued.')
+    queuedHandshakes.push({
+      handshake: req.body.handshake,
+      handshakeResponse: null
+    })
+    res.status(200).send('Handshake queued.')
+  } else {
+    res.status(409).send('Handshake already in the queue.')
   }
-  res.send(409, 'Handshake already in the queue.')
+})
+
+router.post('/response', (req, res) => {
+  const {
+    handshake,
+    handshakeResponse
+  } = req.body
+  const index = findIndex(queuedHandshakes, {
+    handshake
+  })
+  if (!queuedHandshakes[0].handshakeResponse) {
+    queuedHandshakes.splice(index, 1, {
+      handshake,
+      handshakeResponse
+    })
+  }
+  console.log(queuedHandshakes)
+  res.send('Handshake response')
 })
 
 router.get('/requested', (req, res) => {
